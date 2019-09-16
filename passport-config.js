753acd975a985f4d82c58e64a3cc59c9e7d3c718
Garmunganga <Garmunganga@gmail.com>
2019-09-16 18:54:07 +0300

    reset

--- passport-config.js
+++ passport-config.js
@@ -1,5 +1,5 @@
 const LocalStrategy = require('passport-local').Strategy
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcrypt')
 
 function initialize(passport, getUserByEmail, getUserById) {
   const authenticateUser = async (email, password, done) => {
